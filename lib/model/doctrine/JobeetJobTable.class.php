40f951d272aad09fcf675c51802ddedf3219e3a8
amirrahbaran <amirrezarahbaran@gmail.com>
2017-02-05 13:08:43 +0330

    In the case of each job has been shown under its category, the conditions must be appended to upper counterpart and list the jobs with their categories properly.

--- lib/model/doctrine/JobeetJobTable.class.php
+++ lib/model/doctrine/JobeetJobTable.class.php
@@ -17,11 +17,15 @@ class JobeetJobTable extends Doctrine_Table
         return Doctrine_Core::getTable('JobeetJob');
     }
 
-    public function getActiveJobs()
+    public function getActiveJobs(Doctrine_Query $q = null)
     {
-        $q = $this->createQuery('j')
-            ->where('j.expires_at > ?', date('Y-m-d H:i:s', time()))
-            ->orderBy('j.expires_at DESC');
+        if (is_null($q))
+        {
+            $q = Doctrine_Query::create()
+                ->from('JobeetJob j');
+        }
+        $q->andWhere('j.expires_at > ?', date('Y-m-d H:i:s', time()))
+            ->addOrderBy('j.expires_at DESC');
 
         return $q->execute();
     }
