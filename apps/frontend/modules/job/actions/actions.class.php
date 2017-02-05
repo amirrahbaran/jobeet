327938af8ee61deee38df8361be21a17c6174eab
amirrahbaran <amirrezarahbaran@gmail.com>
2017-02-05 11:03:38 +0330

    This refactoring has several benefits over the previous code:
       -  The logic to get the active jobs is now in the Model, where it belongs
       -  The code in the controller is thinner and much more readable
       -  The getActiveJobs() method is re-usable (for instance in another action)
       -  The model code is now unit testable

--- apps/frontend/modules/job/actions/actions.class.php
+++ apps/frontend/modules/job/actions/actions.class.php
@@ -4,12 +4,9 @@ class jobActions extends sfActions
 {
     public function executeIndex(sfWebRequest $request)
     {
-        $q = Doctrine_Query::create()
-            ->from('JobeetJob j')
-            ->where('j.expires_at > ?', date('Y-m-d H:i:s', time()));
-
-        $this->jobeet_jobs = $q->execute();
+        $this->jobeet_jobs = Doctrine_Core::getTable('JobeetJob')->getActiveJobs();
     }
+
     public function executeShow(sfWebRequest $request)
     {
         $this->job = $this->getRoute()->getObject();
--- lib/model/doctrine/JobeetJobTable.class.php
+++ lib/model/doctrine/JobeetJobTable.class.php
@@ -16,4 +16,13 @@ class JobeetJobTable extends Doctrine_Table
     {
         return Doctrine_Core::getTable('JobeetJob');
     }
+
+    public function getActiveJobs()
+    {
+        $q = $this->createQuery('j')
+            ->where('j.expires_at > ?', date('Y-m-d H:i:s', time()))
+            ->orderBy('j.expires_at DESC');
+
+        return $q->execute();
+    }
 }
