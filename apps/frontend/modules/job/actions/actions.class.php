a0280a2a4e026ffd3331ae35f6e9db5d8d9ce224
amirrahbaran <amirrezarahbaran@gmail.com>
2017-02-05 09:57:03 +0330

    Specifying where condition which means that all the records are retrieved from the database.

--- apps/frontend/modules/job/actions/actions.class.php
+++ apps/frontend/modules/job/actions/actions.class.php
@@ -4,9 +4,11 @@ class jobActions extends sfActions
 {
     public function executeIndex(sfWebRequest $request)
     {
-        $this->jobeet_jobs = Doctrine::getTable('JobeetJob')
-            ->createQuery('a')
-            ->execute();
+        $q = Doctrine_Query::create()
+            ->from('JobeetJob j')
+            ->where('j.created_at > ?', date('Y-m-d H:i:s', time() - 86400 * 30));
+
+        $this->jobeet_jobs = $q->execute();
     }
     public function executeShow(sfWebRequest $request)
     {
