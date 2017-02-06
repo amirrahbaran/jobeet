c5c3a1c0cabe311e328e43a33f6b54e6ebf033ce
amirrahbaran <amirrezarahbaran@gmail.com>
2017-02-06 07:58:33 +0330

    Unit  tests written with messages  for slugify method

--- test/unit/JobeetTest.php
+++ test/unit/JobeetTest.php
@@ -4,9 +4,10 @@
 require_once dirname(__FILE__).'/../bootstrap/unit.php';
 
 $t = new lime_test(6);
-$t->is(Jobeet::slugify('Sensio'), 'sensio');
-$t->is(Jobeet::slugify('sensio labs'), 'sensio-labs');
-$t->is(Jobeet::slugify('sensio   labs'), 'sensio-labs');
-$t->is(Jobeet::slugify('paris,france'), 'paris-france');
-$t->is(Jobeet::slugify('  sensio'), 'sensio');
-$t->is(Jobeet::slugify('sensio  '), 'sensio');
+$t->comment('::slugify()');
+$t->is(Jobeet::slugify('Sensio'), 'sensio', '::slugify() converts all characters to lower case');
+$t->is(Jobeet::slugify('sensio labs'), 'sensio-labs', '::slugify() replaces a white space by a -');
+$t->is(Jobeet::slugify('sensio   labs'), 'sensio-labs', '::slugify() replaces several white spaces by a single -');
+$t->is(Jobeet::slugify('  sensio'), 'sensio', '::slugify() removes - at the beginning of a string');
+$t->is(Jobeet::slugify('sensio  '), 'sensio', '::slugify() removes - at the end of a string');
+$t->is(Jobeet::slugify('paris,france'), 'paris-france', '::slugify() replaces non-ASCII characters by a -');
