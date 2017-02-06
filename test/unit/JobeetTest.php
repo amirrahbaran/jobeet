dbf69c3f4d6b1688c3ece8f40385ed0839a14522
amirrahbaran <amirrezarahbaran@gmail.com>
2017-02-06 07:54:07 +0330

    Unit  tests written for slugify method

--- test/unit/JobeetTest.php
+++ test/unit/JobeetTest.php
@@ -3,5 +3,10 @@
 // test/unit/JobeetTest.php
 require_once dirname(__FILE__).'/../bootstrap/unit.php';
 
-$t = new lime_test(1);
-$t->pass('This test always passes.');
+$t = new lime_test(6);
+$t->is(Jobeet::slugify('Sensio'), 'sensio');
+$t->is(Jobeet::slugify('sensio labs'), 'sensio-labs');
+$t->is(Jobeet::slugify('sensio   labs'), 'sensio-labs');
+$t->is(Jobeet::slugify('paris,france'), 'paris-france');
+$t->is(Jobeet::slugify('  sensio'), 'sensio');
+$t->is(Jobeet::slugify('sensio  '), 'sensio');
