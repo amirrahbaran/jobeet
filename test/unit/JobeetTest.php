7183a671015c1d8f50e41161aab3b240af455b38
amirrahbaran <amirrezarahbaran@gmail.com>
2017-02-06 08:39:55 +0330

    The slugify method has a problem to translating the white space to non-white-space. Firstly  its unit test written and then the application logic change to the jobett::slugify method

--- test/unit/JobeetTest.php
+++ test/unit/JobeetTest.php
@@ -3,7 +3,7 @@
 // test/unit/JobeetTest.php
 require_once dirname(__FILE__).'/../bootstrap/unit.php';
 
-$t = new lime_test(6);
+$t = new lime_test(7);
 $t->comment('::slugify()');
 $t->is(Jobeet::slugify('Sensio'), 'sensio', '::slugify() converts all characters to lower case');
 $t->is(Jobeet::slugify('sensio labs'), 'sensio-labs', '::slugify() replaces a white space by a -');
@@ -11,3 +11,4 @@ $t->is(Jobeet::slugify('sensio   labs'), 'sensio-labs', '::slugify() replaces se
 $t->is(Jobeet::slugify('  sensio'), 'sensio', '::slugify() removes - at the beginning of a string');
 $t->is(Jobeet::slugify('sensio  '), 'sensio', '::slugify() removes - at the end of a string');
 $t->is(Jobeet::slugify('paris,france'), 'paris-france', '::slugify() replaces non-ASCII characters by a -');
+$t->is(Jobeet::slugify(''), 'n-a', '::slugify() converts the empty string to n-a');
